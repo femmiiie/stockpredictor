f8210d218f0a0c16026e52e20a018172ed9dd024
Sandro Mocevic <sandro.mocev@gmail.com>
2024-10-24 14:41:06 -0400

    basic frontend test code

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-  pass
+import dearpygui.dearpygui as gui
 
+
+def main():
+  gui.create_context()
+  gui.create_viewport(title="COP3530 Project 3")
+  gui.setup_dearpygui()
+
+  with gui.window(tag="Primary"):
+    gui.add_text("Project 3")
+
+  gui.show_viewport()
+  gui.set_primary_window("Primary", True)
+  gui.start_dearpygui()
+  gui.destroy_context()
 
 
 if __name__ == "__main__":
---
+++ todo_list.md
@@ -0,0 +1,1 @@
+RUN 'pip install dearpygui' FOR IT TO WORK
