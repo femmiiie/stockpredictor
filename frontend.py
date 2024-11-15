859462c84290c3f45f31ff290cc345382737e1d0
Sandro Mocevic <sandro.mocev@gmail.com>
2024-11-15 00:28:15 -0500

    basic date range selector working

--- frontend.py
+++ frontend.py
@@ -2,6 +2,7 @@ import dearpygui.dearpygui as gui
 from globals import *
 from trie import *
 from time import sleep
+from datetime import datetime, timedelta
 
 def render_front(stocks : Trie):
   gui.create_context()
@@ -25,15 +26,15 @@ def render_front(stocks : Trie):
   gui.destroy_context()
 
 def button_setup():
-  button_size = [150, 80]
+  button_size = [150, 60]
   button_pos = [screen_size["width"] - button_size[0] - 50, screen_size["height"] - button_size[1] - 60]
   button = gui.add_button(label="Predict!", width=button_size[0], height=button_size[1])
   gui.set_item_pos(button, button_pos)
 
 def selector_setup(stocks : Trie):
   gui.add_text("Select a Stock:")
-  gui.add_input_text(tag="Search", hint="Type to search...", width=200)
-  gui.add_listbox(tag="Dropdown", items=stocks.get_searched_list(""), callback=select_item, show=False, width=200)
+  gui.add_input_text(tag="search", hint="Type to search...", width=210)
+  gui.add_listbox(tag="dropdown", items=stocks.get_searched_list(""), callback=select_item, show=False, width=210)
 
   with gui.handler_registry():
     gui.add_key_release_handler(callback=(lambda: filter_options(stocks)))
@@ -42,28 +43,51 @@ def selector_setup(stocks : Trie):
     gui.add_item_active_handler(callback=show_listbox)
     gui.add_item_deactivated_handler(callback=hide_listbox)
 
-  gui.bind_item_handler_registry("Search", "search_reg")
+  gui.bind_item_handler_registry("search", "search_reg")
 
 def range_setup():
-  pass
+  gui.add_text("")
+  gui.add_text("Prediction Range:")
+
+  with gui.group(horizontal=True):
+    gui.add_input_text(tag="start_date", default_value=datetime.now().strftime("%m/%d/%Y"), width=150)
+    gui.add_text("-")
+    gui.add_input_text(tag="end_date", default_value=(datetime.now() + timedelta(days=7)).strftime("%m/%d/%Y"), width=150)
+
+  with gui.item_handler_registry(tag="range_reg") as handler:
+    gui.add_item_deactivated_handler(callback=update_date_range)
+
+  gui.bind_item_handler_registry("start_date", "range_reg")
+  gui.bind_item_handler_registry("end_date", "range_reg")
 
 def visualizer_setup():
   pass
 
 def filter_options(stocks : Trie):
-  input_text : str = gui.get_value("Search").upper()
+  input_text : str = gui.get_value("search").upper()
   filtered_items = [item for item in sorted(stocks.get_searched_list(input_text)) if input_text in item]
-  gui.configure_item("Dropdown", items=filtered_items)
+  gui.configure_item("dropdown", items=filtered_items)
   #print(filtered_items)
 
 def show_listbox():
-  gui.show_item("Dropdown")
+  gui.show_item("dropdown")
 
 def hide_listbox():
   sleep(0.1)
-  gui.hide_item("Dropdown")
+  gui.hide_item("dropdown")
 
 def select_item(sender):
-  selected_item = gui.get_value(sender)
-  gui.set_value("Search", selected_item)
-  # gui.hide_item("Dropdown")
+  text = gui.get_value(sender)
+  gui.set_value("search", text)
+
+def update_date_range(sender):
+    start_val = gui.get_value("start_date")
+    end_val = gui.get_value("end_date")
+    date_format = "%m/%d/%Y"
+
+    start_date = datetime.strptime(start_val, date_format)
+    end_date = datetime.strptime(end_val, date_format)
+
+    if start_date > end_date:
+      gui.set_value("start_date", end_date.strftime(date_format))
+      gui.set_value("end_date", start_date.strftime(date_format))    
