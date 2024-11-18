acce04dbc737f4621e0598a861306ddce4fdf506
Sandro Mocevic <sandro.mocev@gmail.com>
2024-11-17 19:55:35 -0500

    updated main placeholder for updated trie code

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ import classes.trie as trie
 
 def main():
   #placeholder values to test search and selection functionality
+  arr = ["word", "ward", "wart", "worry", "rat"]
   placeholder = trie.Trie()
-  placeholder.insert("word")
-  placeholder.insert("ward")
-  placeholder.insert("wart")
-  placeholder.insert("worry")
-  placeholder.insert("rat")
+  placeholder.insert_arr(arr)
 
   frontend.render_front(placeholder)
 
+  #preferred data format for proccessing
+  # list[str] of each name to pass into Trie
+  # list[HashMap] for the daily info of each stock
+  # keep the list 1-to-1 for easy indexing 
 
 if __name__ == "__main__":
   main()
