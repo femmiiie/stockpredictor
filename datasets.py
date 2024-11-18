acac5c3cf75e2628ddf54c6fe9e0cc61b14dca42
Aimar99 <aimarmurua03@gmail.com>
2024-11-18 14:56:50 -0500

    adding stokcs

--- datasets.py
+++ datasets.py
@@ -2,23 +2,33 @@ from classes.hashmap import *
 from datetime import datetime
 # write in terminal pip install yfinance
 import yfinance as yf
+import pandas as pd
 
-data = yf.download('AAPL', start='2013-01-01', end='2013-01-31')
 
-apple = HashMap()
-for i,k in enumerate(data.index):
-    value = []
-    date_string = str(k)
-    parsed_date = datetime.fromisoformat(date_string)
-    cleaned_date = parsed_date.strftime('%Y-%m-%d')
-    for j in data.iloc[i]:
-        value.append(j)
+url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+sp500_table = pd.read_html(url)
+sp500_tickers = sp500_table[0]['Symbol'].tolist()
+stocks_size = len(sp500_tickers)
 
-    apple.put(cleaned_date,value)
+data = yf.download(sp500_tickers, start='2022-01-01', end='2022-01-31', group_by='ticker')
+stocks_list = HashMap(stocks_size)
 
-print(len(apple))
-print(apple.get('2013-01-02'))
+for i in sp500_tickers:
+    data_stock = data[i]
+    size = len(data_stock)
+    stock = HashMap(size)
+    for i,k in enumerate(data_stock.index):
+        value = []
+        date_string = str(k)
+        parsed_date = datetime.fromisoformat(date_string)
+        cleaned_date = parsed_date.strftime('%Y-%m-%d')
+        for j in data_stock.iloc[i]:
+            value.append(j)
 
-stock = {"APPL": [[]], "TSLA": [[]]}
+        stock.put(cleaned_date,value)
+
+    stocks_list.put(i,stock)
+
+print(stocks_list.get("AAPL"))
 
 
